c95a5e24eb08d8e25d20f74152324d2f35e4ecf7
Ione Souza Junior <junior@ionixjunior.com.br>
2020-09-05 10:45:54 -0300

    Iniciar projeto de exemplo

---
+++ TechSaturday/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TechSaturday
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            AppCenter.Start(
+                "android=a3923c0d-ddd9-4240-b48c-3dbe38d096ba",
+                typeof(Analytics),
+                typeof(Crashes)
+            );
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ TechSaturday/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AppCenter.Analytics;
+using Xamarin.Forms;
+
+namespace TechSaturday
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void VerParticipantesClicked(System.Object sender, System.EventArgs e)
+        {
+            Analytics.TrackEvent("Ver participantes");
+        }
+
+        void NaoCliqueClicked(System.Object sender, System.EventArgs e)
+        {
+            var random = new Random();
+            if (random.Next(1, 10) <= 2)
+                throw new Exception("Falei para não clicar!");
+
+            Analytics.TrackEvent("Não clique!");
+        }
+
+        void VerOnlineClicked(System.Object sender, System.EventArgs e)
+        {
+            Analytics.TrackEvent("Ver online");
+        }
+    }
+}
